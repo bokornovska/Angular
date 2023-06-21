5b04eb325e1e36894e5f01d715496dc942a478b5
Borislava Kornovska <105539662+bokornovska@users.noreply.github.com>
2023-06-21 14:09:27 +0300

    Intro to Angular and TypeScript

---
+++ 01. Intro to Angular and TypeScript/TypeScript/playground.ts
@@ -0,0 +1,15 @@
+type User1 = {
+    name: string;
+    age: number;
+};
+
+type User2 = {
+    name: string;
+    age: number;
+};
+
+class UserTest implements User2 {
+    name: string;
+    age: number;
+}
+
