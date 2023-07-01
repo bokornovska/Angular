3d52362cb7334b64e34a55caf9b751d654b2c575
Borislava Kornovska <105539662+bokornovska@users.noreply.github.com>
2023-07-01 19:23:00 +0300

    types

--- 04. Workshop/demo-app-components/src/app/api.service.ts
+++ 04. Workshop/demo-app-components/src/app/api.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment.development';
-
+import { Theme } from './types/theme';
+import { Post } from './types/post';
 @Injectable({
   providedIn: 'root'
 })
@@ -12,11 +13,11 @@ export class ApiService {
   getPosts(limit?: number) {
     const { appUrl } = environment;
     const limitFilter = limit ? `?limit=${limit}` : '';
-    return this.http.get(`${appUrl}/posts${limitFilter}`);
+    return this.http.get<Post[]>(`${appUrl}/posts${limitFilter}`);
   }
 
   getThemes() {
     const { appUrl } = environment;
-    return this.http.get(`${appUrl}/themes`);
+    return this.http.get<Theme[]>(`${appUrl}/themes`);
   }
 }
