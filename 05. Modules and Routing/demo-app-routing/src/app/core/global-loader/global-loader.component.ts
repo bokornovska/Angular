5adaf6712508323e57620fd963bc7d8e102777e5
Borislava Kornovska <105539662+bokornovska@users.noreply.github.com>
2023-07-06 17:04:24 +0300

    fixed loader

--- 05. Modules and Routing/demo-app-routing/src/app/core/global-loader/global-loader.component.ts
+++ 05. Modules and Routing/demo-app-routing/src/app/core/global-loader/global-loader.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GlobalLoaderService } from './global-loader.service';
 
 @Component({
   selector: 'app-global-loader',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class GlobalLoaderComponent {
 
+  constructor(public globalLoaderService:GlobalLoaderService){
+    
+  }
 }
--- 05. Modules and Routing/demo-app-routing/src/app/user/user-list/user-list.component.ts
+++ 05. Modules and Routing/demo-app-routing/src/app/user/user-list/user-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user.service';
 
 import { User } from '../../types/User';
+import { GlobalLoaderService } from 'src/app/core/global-loader/global-loader.service';
 
 @Component({
   selector: 'app-user-list',
@@ -13,15 +14,20 @@ export class UserListComponent implements OnInit {
   userList: User[] = []
 
 
-  constructor(private userService: UserService) {
+  constructor(private userService: UserService, private globalLoaderService: GlobalLoaderService) {
 
   }
   ngOnInit(): void {
 
-    this.userService.fetchUsers().subscribe(users => {
-      console.log(users);
-      this.userList = users;
-    });
+    this.globalLoaderService.showLoader();
+    setTimeout(() => {
+      this.userService.fetchUsers().subscribe(users => {
+        // console.log(users);
+        this.userList = users;
+        this.globalLoaderService.hideLoader();
+      });
+    }, 3000)
+
   }
 
 }
