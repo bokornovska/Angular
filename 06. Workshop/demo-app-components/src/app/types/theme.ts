1eb0416cc6214033326a2c042f5547ac614d2d94
Borislava Kornovska <105539662+bokornovska@users.noreply.github.com>
2023-07-13 15:54:33 +0300

    posts comments section

--- 06. Workshop/demo-app-components/src/app/types/theme.ts
+++ 06. Workshop/demo-app-components/src/app/types/theme.ts
@@ -1,8 +1,9 @@
 import { UserId } from "./user-id";
+import { Post } from "./post";
 
 export interface Theme {
     "subscribers": string[];
-    "posts": string[];
+    "posts": any;
     "_id": string;
     "themeName": string;
     "userId": UserId;
