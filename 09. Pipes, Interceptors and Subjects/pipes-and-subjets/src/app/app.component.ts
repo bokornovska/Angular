8487169c1c0a559b69e9deebeea156419450399c
Borislava Kornovska <105539662+bokornovska@users.noreply.github.com>
2023-07-25 08:54:10 +0300

    pipes - examples

--- 09. Pipes, Interceptors and Subjects/pipes-and-subjets/src/app/app.component.ts
+++ 09. Pipes, Interceptors and Subjects/pipes-and-subjets/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { interval } from 'rxjs/internal/observable/interval';
+import { map } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-root',
@@ -17,4 +20,20 @@ export class AppComponent {
   sum(a: number, b: number): number {
     return a + b;
   }
+
+  addProperty(): void {
+    (this.person as any)['test123'] = 'test123';
+    console.log(this.person)
+  }
+
+  // p = Promise.resolve(100);
+  p = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(100)
+    }, 3000)
+  });
+
+  time$ = interval(1000).pipe(
+    map(() => new Date())
+  )
 }
--- 09. Pipes, Interceptors and Subjects/pipes-and-subjets/src/app/reduce.pipe.ts
+++ 09. Pipes, Interceptors and Subjects/pipes-and-subjets/src/app/reduce.pipe.ts
@@ -3,7 +3,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({
   name: 'reduce',
   // pure: false => not pure functions will re-render
-  // pure: true => pure function will be memoized
+  // pure: true => pure function will be memoized DEFAULT
 })
 
 export class ReducePipe<T> implements PipeTransform {
@@ -11,6 +11,7 @@ export class ReducePipe<T> implements PipeTransform {
   transform(array: T[], reduceFn: (acc: any, crr: T) => any, initialValue: T): unknown {
     // const sum = (acc, crr) => acc+crr;
     // [1, 2, 3, 4].reduce(sum, 0)
+    console.log('Invoked from ReducePipe')
     return array.reduce(reduceFn, initialValue);
   }
 
