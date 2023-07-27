e9dc890df269b8e2a5e5aac0f79a880b27d28c6b
Borislava Kornovska <105539662+bokornovska@users.noreply.github.com>
2023-07-27 09:26:55 +0300

    promise and observable demo

--- 09. Pipes, Interceptors and Subjects/pipes-and-subjets/src/main.ts
+++ 09. Pipes, Interceptors and Subjects/pipes-and-subjets/src/main.ts
@@ -1,7 +1,38 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { Observable, map } from 'rxjs';
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+
+//Promise demo
+
+// const p = new Promise(resolve => {
+//   resolve(100);
+// });
+
+// p.then(console.log);
+
+//Observable demo
+
+// const o$ = new Observable<number>(obs => {
+//   obs.next(1000);
+//   obs.next(2000);
+//   obs.next(3000);
+
+//   obs.error(new Error('error here'));
+
+//   return () => {
+//     //cleanup
+//   }
+
+// });
+
+// o$.pipe(map((num: number) => num + 1)).subscribe({
+//   next:console.log,
+//   error: (err) => console.error(`Error ${err}`),
+//   complete: () => console.log('Observable completed')
+// })
