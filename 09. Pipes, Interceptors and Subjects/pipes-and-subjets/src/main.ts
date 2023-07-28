db0c5900c4ad29f8dae14e95bce46c1879208932
Borislava Kornovska <105539662+bokornovska@users.noreply.github.com>
2023-07-28 14:33:41 +0300

    async subject - demo

--- 09. Pipes, Interceptors and Subjects/pipes-and-subjets/src/main.ts
+++ 09. Pipes, Interceptors and Subjects/pipes-and-subjets/src/main.ts
@@ -1,7 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-import { BehaviorSubject, Observable, ReplaySubject, Subject, map } from 'rxjs';
+// import { AsyncSubject, BehaviorSubject, Observable, ReplaySubject, Subject, map } from 'rxjs';
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
@@ -73,48 +73,67 @@ platformBrowserDynamic().bootstrapModule(AppModule)
 
 // SUBJECTS
 
-const subj$$ = new Subject();
+// const subj$$ = new Subject();
 
-subj$$.subscribe(console.log); // A
+// subj$$.subscribe(console.log); // A
 
-subj$$.next(123); // A
+// subj$$.next(123); // A
 
-subj$$.subscribe(console.log); // B
-subj$$.subscribe(console.log); // C
-subj$$.next(400); // A, B, C
+// subj$$.subscribe(console.log); // B
+// subj$$.subscribe(console.log); // C
+// subj$$.next(400); // A, B, C
 
-setTimeout(() => {
-  subj$$.subscribe(console.log); // D
-  subj$$.next(100) // A, B, C, D
-}, 2000)
+// setTimeout(() => {
+//   subj$$.subscribe(console.log); // D
+//   subj$$.next(100) // A, B, C, D
+// }, 2000)
 
 
 
 // Behavior Seubject
 
-const bSubj$$ = new BehaviorSubject(100);
-bSubj$$.subscribe((data) => console.log(`Subscription 1:`, data));
+// const bSubj$$ = new BehaviorSubject(100);
+// bSubj$$.subscribe((data) => console.log(`Subscription 1:`, data));
 
-setTimeout(() => {
-  bSubj$$.subscribe((data) => console.log('Subscription 2: ', data));
-  bSubj$$.next(200);
+// setTimeout(() => {
+//   bSubj$$.subscribe((data) => console.log('Subscription 2: ', data));
+//   bSubj$$.next(200);
 
-  setTimeout(() => {
-    bSubj$$.subscribe((data) => console.log('Subscription 3: ', data));
-    bSubj$$.next(300);
-  })
-}, 2000)
+//   setTimeout(() => {
+//     bSubj$$.subscribe((data) => console.log('Subscription 3: ', data));
+//     bSubj$$.next(300);
+//   })
+// }, 2000)
 
 // Replay Subject
 
-const rSubj$$ = new ReplaySubject(10);
-rSubj$$.next(1000);
-rSubj$$.subscribe((d) => console.log('Subscribe 1', d))
+// const rSubj$$ = new ReplaySubject(10);
+// rSubj$$.next(1000);
+// rSubj$$.subscribe((d) => console.log('Subscribe 1', d))
 
-for (let i = 1; i <= 30; i++) {
+// for (let i = 1; i <= 30; i++) {
 
-  rSubj$$.next( i)
-}
+//   rSubj$$.next( i)
+// }
 
-console.log(`-----------------------`)
-rSubj$$.subscribe(console.log)
+// console.log(`-----------------------`)
+// rSubj$$.subscribe(console.log)
+
+
+//ASYNC Subjects
+
+// const aSubj$$ = new AsyncSubject();
+
+// aSubj$$.next(1);
+// aSubj$$.next(2);
+// aSubj$$.next(3);
+
+// aSubj$$.subscribe((d) => console.log('Subscribe 1', d));
+
+// aSubj$$.next(5);
+
+// aSubj$$.subscribe((d) => console.log('Subscribe 2', d));
+
+// aSubj$$.complete();
+
+
