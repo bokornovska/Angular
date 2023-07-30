3add80b05b50b34b0706a74b199b531517105aa9
Borislava Kornovska <105539662+bokornovska@users.noreply.github.com>
2023-07-30 09:47:33 +0300

    new-theme component

--- 10. Workshop - subjects/demo-app-components/src/app/api.service.ts
+++ 10. Workshop - subjects/demo-app-components/src/app/api.service.ts
@@ -11,12 +11,15 @@ export class ApiService {
 
   constructor(private http: HttpClient) { }
 
+  // POSTS
   getPosts(limit?: number) {
     const { appUrl } = environment;
     const limitFilter = limit ? `?limit=${limit}` : '';
     return this.http.get<Post[]>(`${appUrl}/posts${limitFilter}`);
   }
 
+
+  // THEME
   getThemes() {
     const { appUrl } = environment;
     return this.http.get<Theme[]>(`${appUrl}/themes`);
@@ -25,5 +28,12 @@ export class ApiService {
   getTheme(id:string) {
     const { appUrl } = environment;
     return this.http.get<Theme>(`${appUrl}/themes/${id}`);
+  }
+
+  createTheme(
+    themeName:string,
+    postText: string,
+  ){
+    return this.http.post<Theme>('/api/themes', {themeName, postText})
   }
 }
--- 10. Workshop - subjects/demo-app-components/src/app/theme/new-theme/new-theme.component.ts
+++ 10. Workshop - subjects/demo-app-components/src/app/theme/new-theme/new-theme.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ApiService } from 'src/app/api.service';
 
 @Component({
   selector: 'app-new-theme',
@@ -8,11 +10,17 @@ import { NgForm } from '@angular/forms';
 })
 export class NewThemeComponent {
 
+  constructor(private apiService:ApiService, private router:Router){
+
+  }
 
   newThemeSubmitHandler(form: NgForm): void {
 
     if (form.invalid) { return; }
 
-    console.log(form.value);
+    const {themeName, postText} = form.value;
+    this.apiService.createTheme(themeName, postText).subscribe(() => {
+      this.router.navigate(['/themes'])
+    })
   }
 }
